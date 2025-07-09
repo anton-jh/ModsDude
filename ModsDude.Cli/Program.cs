86f8e65b43040bf1bcd9629494153adfc40d5223
Anton J Håkansson <anton.jh@hotmail.com>
2025-07-09 13:11:29 +0200

    stuff

--- ModsDude.Cli/Program.cs
+++ ModsDude.Cli/Program.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ModsDude.Cli.Commands;
 using ModsDude.Cli.DependencyInjection;
 using Spectre.Console;
+using Spectre.Console.Cli;
 
 
-await Host.CreateDefaultBuilder(args)
-    .ConfigureServices(static (ctx, services) =>
-    {
-        services.AddSingleton(AnsiConsole.Console);
-    })
-    .BuildApp()
-    .RunAsync(args);
+var builder = Host.CreateDefaultBuilder(args);
+
+
+builder.ConfigureServices(static (ctx, services) =>
+{
+    services.AddSingleton(AnsiConsole.Console);
+});
+
+
+var registrar = new TypeRegistrar(builder);
+var app = new CommandApp(registrar);
+
+
+app.Configure(config =>
+{
+    config.AddCommand<GreetingCommand>("greet");
+});
+
+
+await app.RunAsync(args);
