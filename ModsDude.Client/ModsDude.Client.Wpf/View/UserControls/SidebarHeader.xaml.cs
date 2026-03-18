6c5d249b67aee17c25b13928b7868a87354d607b
Anton J Håkansson <anton.jh@hotmail.com>
2026-03-18 20:17:55 +0100

    wip

---
+++ ModsDude.Client/ModsDude.Client.Wpf/View/UserControls/SidebarHeader.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ModsDude.Client.Wpf.View.UserControls;
+
+public partial class SidebarHeader : UserControl
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(SidebarHeader),
+            new PropertyMetadata(""));
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register(
+            nameof(Command),
+            typeof(ICommand),
+            typeof(SidebarHeader),
+            new PropertyMetadata(null));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+
+    public static readonly DependencyProperty ActionIconProperty =
+        DependencyProperty.Register(
+            nameof(ActionIcon),
+            typeof(string),
+            typeof(SidebarHeader),
+            new PropertyMetadata(""));
+
+    public string ActionIcon
+    {
+        get => (string)GetValue(ActionIconProperty);
+        set => SetValue(ActionIconProperty, value);
+    }
+
+
+    public SidebarHeader()
+    {
+        InitializeComponent();
+    }
+}
